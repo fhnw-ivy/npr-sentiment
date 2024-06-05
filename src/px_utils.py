2f401cb8d0624ed4aa644a7d35c4e3962b76d69d
Nils <nils.fahrni@students.fhnw.ch>
2024-06-05 10:41:36 +0200

    fixed merging & concluded embedding analysis

--- src/px_utils.py
+++ src/px_utils.py
@@ -11,7 +11,7 @@ DEFAULT_SCHEMA = px.Schema(
         actual_label_column_name="label",
         embedding_feature_column_names={
                 "text_embedding": px.EmbeddingColumnNames(
-                vector_column_name="content_vector", 
+                vector_column_name="embedding", 
                 raw_data_column_name="content",
                 ),
         }
@@ -24,7 +24,7 @@ def create_dataset(name: str,
                    content: np.array = None):
         if content is not None:
                 dataframe['content'] = content
-        dataframe['content_vector'] = embedding_vectors
+        dataframe['embedding'] = embedding_vectors
         if predicted_labels: dataframe['predicted_label'] = predicted_labels
 
         return px.Dataset(dataframe=dataframe, schema=DEFAULT_SCHEMA, name=name)
