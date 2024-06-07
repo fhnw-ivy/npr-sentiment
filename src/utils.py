0dd33468f7417ea2e8c0b19c918ab09b012ae0aa
Dominik Filliger <filligerdominik@gmail.com>
2024-06-07 17:36:57 +0200

    Removed redundant code

--- src/utils.py
+++ src/utils.py
@@ -21,14 +21,6 @@ def load_env():
     load_dotenv()
 
 
-def set_seed(seed: int = 1337):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
-
-
 def get_torch_device():
     if torch.backends.mps.is_available():
         return torch.device("mps")
